da55fd895e3a11e4f866c4561355f94507cef5bc
Roberto Piga <rpiga.net@gmail.com>
2021-02-03 19:51:33 +0100

    Update lines.py

--- consumers/models/lines.py
+++ consumers/models/lines.py
@@ -21,27 +21,51 @@ class Lines:
 
     def process_message(self, message):
         """Processes a station message"""
-        logger.info(f"DEBUG (lines 2) {self.message}")
-##        if "org.chicago.cta.station" in message.topic():
+        print(f"DEBUG {message.topic()}")
         if "com.udacity.station" in message.topic():
-            logger.info(f"DEBUG (lines 3a) {message.topic()}")
             value = message.value()
-##            if message.topic() == "org.chicago.cta.stations.table.v1":
         elif message.topic() == "connect.stations.table":
-            value = json.loads(message)
-            logger.info(f"DEBUG (lines 3b) {value}")
-            if value["line"] == "green":
-                self.green_line.process_message(message)
-            elif value["line"] == "red":
-                self.red_line.process_message(message)
-            elif value["line"] == "blue":
-                self.blue_line.process_message(message)
-            else:
-                logger.debug("discarding unknown line msg %s", value["line"])
-        elif "TURNSTILE_SUMMARY" == message.topic():
-            logger.info("DEBUG (lines 3c) %s", message.topic())
+            value = json.loads(value)
+
+        if value["line"] == "green":
+            self.green_line.process_message(message)
+        elif value["line"] == "red":
+            self.red_line.process_message(message)
+        elif value["line"] == "blue":
+            self.blue_line.process_message(message)
+        else:
+            logger.info("discarding unknown line msg %s", value["line"])
+        
+        if message.topic() == "TURNSTILE_SUMMARY":
             self.green_line.process_message(message)
             self.red_line.process_message(message)
             self.blue_line.process_message(message)
         else:
             logger.info("ignoring non-lines message %s", message.topic())
+            
+            
+#         """Processes a station message"""
+#         logger.info(f"DEBUG (lines 2) {self.message}")
+# ##        if "org.chicago.cta.station" in message.topic():
+#         if "com.udacity.station" in message.topic():
+#             logger.info(f"DEBUG (lines 3a) {message.topic()}")
+#             value = message.value()
+# ##            if message.topic() == "org.chicago.cta.stations.table.v1":
+#         elif message.topic() == "connect.stations.table":
+#             value = json.loads(message)
+#             logger.info(f"DEBUG (lines 3b) {value}")
+#             if value["line"] == "green":
+#                 self.green_line.process_message(message)
+#             elif value["line"] == "red":
+#                 self.red_line.process_message(message)
+#             elif value["line"] == "blue":
+#                 self.blue_line.process_message(message)
+#             else:
+#                 logger.debug("discarding unknown line msg %s", value["line"])
+#         elif "TURNSTILE_SUMMARY" == message.topic():
+#             logger.info("DEBUG (lines 3c) %s", message.topic())
+#             self.green_line.process_message(message)
+#             self.red_line.process_message(message)
+#             self.blue_line.process_message(message)
+#         else:
+#             logger.info("ignoring non-lines message %s", message.topic())
